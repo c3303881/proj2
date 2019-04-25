f2337afe8d57f9083a1b1f0be5a1eb764a97e197
Talesin Court-Kriesch <c3303881@uon.edu.au>
2019-04-25 05:02:43 +0000

    printing

--- moneymaker.c
+++ moneymaker.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
-int i, x, c;
+int i, x, key;
+
+char c, m; //m=message number
+
+scanf("%c",&c);
+
+c = c - 65;
 
 char str[26];
 //printf("%s\n", str);
 //scanf("%s", str);
  
+//encryption eqt for rotation
+//encryption = ((m + key) % 26);
+//decryption eqt for rotation
+//decryption = ((c - key) % 26);
+printf("please enter 'key'\n");
+scanf("%d", &key);
 int main() {
+    //rotation cypher
+    printf("please select function. \n");
+    printf("1 = encryption cypher. \n");                            
+    printf("2 = decryption cypher. \n");
+    scanf("%d", &x);
+
+
+
 
   return 0;
 }
