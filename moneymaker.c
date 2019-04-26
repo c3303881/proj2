edc677c9d69f0bf996590fe04ca486ea038cc3fe
Talesin Court-Kriesch <c3303881@uon.edu.au>
2019-04-26 01:19:13 +0000

    encryption and decryption functions, don't work yet idk why

--- moneymaker.c
+++ moneymaker.c
@@ -1,35 +1,69 @@
 #include <stdio.h>
-
+#include <strings.h>
 int i, x, key; //key rotation amount
-char c, m; //m=message number
+char; //m=message number
 
-scanf("%c",&c);
+//scanf("%c",&c);
 
 //ASCII to ALPHA
-c = c - 65;
+//c = c - 65;
 
-char str[26];
+char str[100];
 //printf("%s\n", str);
 //scanf("%s", str);
  
 //encryption eqt for rotation
-//encryption = ((m + key) % 26);
+//encryption = ((str + key) % 26);
 //decryption eqt for rotation
 //decryption = ((c - key) % 26);
-printf("please enter 'key'\n");
+printf("please enter 'key'\n", key );
 scanf("%d", &key);
+
+
+
+
+
+
+//encryption function
+void rotation_enc();
+{
+for(i = 0; str[i] != '\0'; i++){
+        
+        if(str[i] >= 65 && str[i] <= 90)
+        str[i] = str[i]-key;
+    }
+    else if(str[i] < 65){
+    str[i] = str[i] + 26;
+    if (str[i] < 58){
+        str[i] = '0';
+    }
+}
+printf("encryption:  %s \n", str);
+}
+
+//decryption function
+void rotation_dec();
+{
+    for(i=0; i < str[i]; i++){
+        str[i]= str[i] - key;
+    }
+}
+
+
+
+
+
 int main() {
     
-    printf("please enter string \n");
-    scanf("%s", string)
+    printf("please enter string \n", str);
+    scanf("%s", str)
     //rotation cypher
-    printf("please select function. \n");
-    printf("1 = encryption cypher. \n");                            
-    printf("2 = decryption cypher. \n");
-    scanf("%d", &x);
+   // printf("please select function. \n");
+    //printf("1 = encryption cypher. \n");                            
+   // printf("2 = decryption cypher. \n");
+    //scanf("%d", &x);
     
-    for(i = 0 < 26 && str[i] != '\0')
-
+    
 
 
 
