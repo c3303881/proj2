eb9ebb1a778a14b94a669682da75ef540efbf2ed
Talesin Court-Kriesch <c3303881@uon.edu.au>
2019-04-29 01:05:55 +0000

    commenting and cleaning of code

--- moneymaker.c
+++ moneymaker.c
@@ -1,3 +1,14 @@
+//ENG1003
+//ASSIGNMENT 1 
+//TALESIN COURT-KRIESCH
+//C3303881
+//PROGRAM THAT TAKES A MESSAGE, ENCRYPTS IT AND DECRYPTS IT AGAIN USING ROTATION AND SUBSTITUTION CYPHERS
+//LAB THURS 2-5
+
+
+
+
+
 #include <stdio.h>
 #include <strings.h>
 #include <ctype.h>
@@ -19,15 +30,15 @@ char str[1024];
 void rotation_enc();  //func def
 {
     for(i = 0; str[i] != '\0'; i++){   //count loop identifying end 
-       str = toupper (str[i]- 'A');
-         if(str[i] >= 'A' && str[i] <= 'Z'){//values A-Z in ASCI
-           str[i] = str[i] - key;
-         }//moving letters
+       str = toupper (str[i]- 'A');    //toupper function to convert lowercase letters to upper
+         if(str[i] >= 'A' && str[i] <= 'Z'){//values A-Z in ASCI     
+           str[i] = str[i] - key;                                      // couldnt figure out computation with % 26, using loop
+         }//moving letters                                             //counting between 65-90 ascii values A and Z
           if(str[i] < 65){  //only caps needed 
            str[i] = str[i]+26;
            
-             if (str[i] == 32){
-             str[i] = '0';
+             if (str[i] == '32'){     //if character in string is equal to ascii 32 print space
+             str[i] = ' ';
             }
         }
     }
@@ -35,7 +46,7 @@ printf("encryption:  %s \n", str);
 
 }
 
-
+  
 // rotation decryption function
 
 void rotation_dec();
@@ -47,14 +58,20 @@ void rotation_dec();
           if(str[i] > 90){  //only caps needed 
            str[i] = str[i]-26;
            
-              if (str[i] == 32){
-             str[i] = '0';
+              if (str[i] == '32'){
+             str[i] = ' ';
              }
           }
     }
     printf("decryption is; %s \n",str);
 
 }
+//BOTH ROTATION ENCRYPTION AND DECRYPTION WORK ON THEIR OWN
+// WITH INCLUSION OF  printf("please enter string \n", str);
+  // scanf("%s", str)
+  //  printf("please enter 'key' 0-26 \n", key );
+  //  scanf("%d", &key);
+ // IN THE MAIN
 
 // substitution encryption function
 void substitution_enc();{
@@ -67,14 +84,16 @@ void substitution_enc();{
            str[i]= str[i]
        }
    }
-    
+    //initialysing substituted/encrypted characters 
     char encrypted[26] = {'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'};
+    printf("encryption is:  %s\n", str);
 }
 
 // substitution decryption function
 void substitution_dec();{
     
     char encrypted[26] = {'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'};
+    printf("decryption is:  %s\n", str);
 }
 
 
@@ -86,8 +105,8 @@ int main() {
     scanf("%d", &key);
     
     // switch case to pick functions
-     while(1){
-    printf("please choose a function \n" );
+     while(1){                                       // user interface using printf and scanf within a switchcase...
+    printf("please choose a function \n" );          // that calls the seperate encryption/decryption functions when selected
     printf(" '1' rotation encryption \n");
     printf(" '2' rotation decryption \n");
     printf(" '3' substitution encryption \n");
@@ -96,7 +115,7 @@ int main() {
    scanf("%d", &choice) 
     
     switch(choice){                                    //switch case for selection of function 
-        case 1: void rotation_enc();
+        case 1: void rotation_enc();                   
         break;
         case 2: void rotation_dec();
         break;
